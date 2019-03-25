5033474224e84eef1b5caadfdb70d1013d1cba20
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-25 16:10:40 +0530

    p56.c

---
+++ p56.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ #include<string.h>
+ int main()
+ {
+ char str[20],count;
+ int i;
+ scanf("%s %c",str,&c);
+ for(i=0;str[i]='\0';i++)
+ {
+ printf("%d",i++)
+ return 0;
+ }
+  
