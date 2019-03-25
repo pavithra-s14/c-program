1ddeeba7090904bb401cf968c645108f3ad51e20
pavithra-s14 <47560863+pavithra-s14@users.noreply.github.com>
2019-03-25 20:19:12 +0530

    p63.c

---
+++ p63.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int a[20],b[20],i,j;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",a[i]);
+}
+for(i=0;i<n;i++)
+{
+scanf("%d",b[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++
+{
+if(a[i]==b[i]);
+{
+printf("%d",a[i]);
+b[j]='+';
+break;
+}
+}
+}
+return 0;
+}
